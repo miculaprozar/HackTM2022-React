861fb472a125afd222bec19a42d02f98f3d7dd4f
Bogdan Falk <bogdan37falk@gmail.com>
2022-06-11 16:07:35 +0300

    Login

--- src/views/Auth/SignIn.js
+++ src/views/Auth/SignIn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { apiFactory } from '../../api_factory/index.ts';
+import {apiFactory} from '../../api_factory/index.ts';
 // Chakra imports
 import {
   Box,
@@ -15,91 +15,113 @@ import {
   useColorModeValue,
   FormErrorMessage,
 } from '@chakra-ui/react';
-import { useForm } from 'react-hook-form';
+import {useForm} from 'react-hook-form';
 // Assets
 import signInImage from 'assets/img/signInImage.png';
+import {useHistory} from 'react-router-dom';
 
 function SignIn() {
   // Chakra color mode
   const titleColor = useColorModeValue('teal.300', 'teal.200');
   const textColor = useColorModeValue('gray.400', 'white');
 
+  const history = useHistory();
+
   const {
     handleSubmit,
     register,
-    formState: { errors, isSubmitting },
+    formState: {errors, isSubmitting},
   } = useForm();
 
-  const getUser = async () => {
-    const user = await apiFactory().data.account().getUser();
-    console.log(user);
-  };
-  function onSubmit(values) {
-    getUser();
-    console.log(user);
-    console.log('the submit values:', values);
+  // const getUser = async () => {
+  //   const user = await apiFactory().data.account().getUser();
+  //   console.log(user);
+  // };
 
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        alert(JSON.stringify(values, null, 2));
-        resolve();
-      }, 3000);
-    });
+  const tryLogin = async (values) => {
+    try {
+      const token = await apiFactory().data.account().login({
+        email: values.email,
+        password: values.password,
+      });
+      if (typeof token === 'string') {
+        localStorage.setItem('token', token);
+        const userDataArray = await apiFactory()
+          .data.account()
+          .getCurrentUser();
+        const userData = userDataArray[0];
+        console.log(userData);
+        if (userData.roleId === 1) {
+          history.push('/wholesale/profile');
+        } else if (userData.roleId === 2) {
+          history.push('/producer/profile');
+        } else throw new Error('User role not found');
+      } else throw new Error('Date Invalide');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  function onSubmit(values) {
+    // getUser();
+
+    console.log('the submit values:', values);
+    tryLogin(values);
   }
   return (
-    <Flex position='relative' mb='40px'>
+    <Flex position="relative" mb="40px">
       <Flex
-        h={{ sm: 'initial', md: '75vh', lg: '85vh' }}
-        w='100%'
-        maxW='1044px'
-        mx='auto'
-        justifyContent='space-between'
-        mb='30px'
-        pt={{ sm: '100px', md: '0px' }}
+        h={{sm: 'initial', md: '75vh', lg: '85vh'}}
+        w="100%"
+        maxW="1044px"
+        mx="auto"
+        justifyContent="space-between"
+        mb="30px"
+        pt={{sm: '100px', md: '0px'}}
       >
         <Flex
-          alignItems='center'
-          justifyContent='start'
-          style={{ userSelect: 'none' }}
-          w={{ base: '100%', md: '50%', lg: '42%' }}
+          alignItems="center"
+          justifyContent="start"
+          style={{userSelect: 'none'}}
+          w={{base: '100%', md: '50%', lg: '42%'}}
         >
           <Flex
-            direction='column'
-            w='100%'
-            background='transparent'
-            p='48px'
-            mt={{ md: '150px', lg: '80px' }}
+            direction="column"
+            w="100%"
+            background="transparent"
+            p="48px"
+            mt={{md: '150px', lg: '80px'}}
           >
-            <Heading color={titleColor} fontSize='32px' mb='10px'>
+            <Heading color={titleColor} fontSize="32px" mb="10px">
               Welcome Back
             </Heading>
             <Text
-              mb='36px'
-              ms='4px'
+              mb="36px"
+              ms="4px"
               color={textColor}
-              fontWeight='bold'
-              fontSize='14px'
+              fontWeight="bold"
+              fontSize="14px"
             >
               Enter your email and password to sign in
             </Text>
             <form onSubmit={handleSubmit(onSubmit)}>
               <FormControl isInvalid={errors.email}>
                 <FormLabel
-                  ms='4px'
-                  fontSize='sm'
-                  fontWeight='normal'
-                  htmlFor='email'
+                  ms="4px"
+                  fontSize="sm"
+                  fontWeight="normal"
+                  htmlFor="email"
                 >
                   Email
                 </FormLabel>
                 <Input
                   mb={`${!errors.email ? '24px' : '0px'}`}
-                  borderRadius='15px'
-                  fontSize='sm'
-                  type='text'
-                  placeholder='Your email adress'
-                  size='lg'
-                  id='email'
+                  borderRadius="15px"
+                  fontSize="sm"
+                  type="text"
+                  placeholder="Your email adress"
+                  size="lg"
+                  id="email"
                   {...register('email', {
                     required: 'This is required',
                     minLength: {
@@ -108,27 +130,27 @@ function SignIn() {
                     },
                   })}
                 />
-                <FormErrorMessage mb='24px'>
+                <FormErrorMessage mb="24px">
                   {errors.email?.message}
                 </FormErrorMessage>
               </FormControl>
               <FormControl isInvalid={errors.password}>
                 <FormLabel
-                  ms='4px'
-                  fontSize='sm'
-                  fontWeight='normal'
-                  htmlFor='password'
+                  ms="4px"
+                  fontSize="sm"
+                  fontWeight="normal"
+                  htmlFor="password"
                 >
                   Password
                 </FormLabel>
                 <Input
-                  borderRadius='15px'
+                  borderRadius="15px"
                   mb={`${!errors.password ? '36px' : '0px'}`}
-                  fontSize='sm'
-                  type='password'
-                  placeholder='Your password'
-                  size='lg'
-                  id='password'
+                  fontSize="sm"
+                  type="password"
+                  placeholder="Your password"
+                  size="lg"
+                  id="password"
                   {...register('password', {
                     required: 'This is required',
                     minLength: {
@@ -137,29 +159,29 @@ function SignIn() {
                     },
                   })}
                 />
-                <FormErrorMessage mb='36px'>
+                <FormErrorMessage mb="36px">
                   {errors.password?.message}
                 </FormErrorMessage>
-                <FormControl display='flex' alignItems='center'>
-                  <Switch id='remember-login' colorScheme='teal' me='10px' />
+                <FormControl display="flex" alignItems="center">
+                  <Switch id="remember-login" colorScheme="teal" me="10px" />
                   <FormLabel
-                    htmlFor='remember-login'
-                    mb='0'
-                    ms='1'
-                    fontWeight='normal'
+                    htmlFor="remember-login"
+                    mb="0"
+                    ms="1"
+                    fontWeight="normal"
                   >
                     Remember me
                   </FormLabel>
                 </FormControl>
                 <Button
-                  fontSize='10px'
-                  type='submit'
-                  bg='teal.300'
-                  w='100%'
-                  h='45'
-                  mb='20px'
-                  color='white'
-                  mt='20px'
+                  fontSize="10px"
+                  type="submit"
+                  bg="teal.300"
+                  w="100%"
+                  h="45"
+                  mb="20px"
+                  color="white"
+                  mt="20px"
                   _hover={{
                     bg: 'teal.200',
                   }}
@@ -173,15 +195,15 @@ function SignIn() {
               </FormControl>
             </form>
             <Flex
-              flexDirection='column'
-              justifyContent='center'
-              alignItems='center'
-              maxW='100%'
-              mt='0px'
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              maxW="100%"
+              mt="0px"
             >
-              <Text color={textColor} fontWeight='medium'>
+              <Text color={textColor} fontWeight="medium">
                 Don't have an account?
-                <Link color={titleColor} as='span' ms='5px' fontWeight='bold'>
+                <Link color={titleColor} as="span" ms="5px" fontWeight="bold">
                   Sign Up
                 </Link>
               </Text>
@@ -189,21 +211,21 @@ function SignIn() {
           </Flex>
         </Flex>
         <Box
-          display={{ base: 'none', md: 'block' }}
-          overflowX='hidden'
-          h='100%'
-          w='40vw'
-          position='absolute'
-          right='0px'
+          display={{base: 'none', md: 'block'}}
+          overflowX="hidden"
+          h="100%"
+          w="40vw"
+          position="absolute"
+          right="0px"
         >
           <Box
             bgImage={signInImage}
-            w='100%'
-            h='100%'
-            bgSize='cover'
-            bgPosition='50%'
-            position='absolute'
-            borderBottomLeftRadius='20px'
+            w="100%"
+            h="100%"
+            bgSize="cover"
+            bgPosition="50%"
+            position="absolute"
+            borderBottomLeftRadius="20px"
           ></Box>
         </Box>
       </Flex>
